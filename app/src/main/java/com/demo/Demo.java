161820d40ac54baa226cea6abd562900a3e040a0
Amain <284297969>
2017-08-28 16:22:06 +0800

    初始化导入

---
+++ app/src/main/java/com/demo/Demo.java
@@ -0,0 +1,17 @@
+package com.demo;
+
+import android.app.Activity;
+
+/**
+ * ZMusicPlayer 1.0
+ * Created on 2017/8/28 16:20
+ *
+ * @author Aman
+ * @Email: 1390792438@qq.com
+ */
+
+public class Demo extends Activity {
+    public Demo() {
+        super();
+    }
+}
