c045eb4f20a51f398ac2a6e417e73015449083b8
Amain <284297969>
2017-08-28 16:46:07 +0800

    upload

---
+++ amanquick/src/main/java/com/aman/sql/SQLUtils.java
@@ -0,0 +1,14 @@
+package com.aman.sql;
+
+/**
+ * ZMusicPlayer 1.0
+ * Created on 2017/8/28 16:43
+ *
+ * @author Aman
+ * @Email: 1390792438@qq.com
+ */
+
+public class SQLUtils {
+    public static final String Hello = "hello";
+
+}
